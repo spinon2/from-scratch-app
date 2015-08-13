8974ce4d5ffb84701646650d84d08e1b3e152aa7
David Parks <spinon@industriasframer.com>
2015-08-13 13:26:04 -0700

    Push new structure files and JHint & Watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,21 +1,25 @@
+// 
+// Develop by GD Team SFO CA. 
+// 
+
 module.exports = function(grunt) {
   grunt.initConfig({
 
-      watch:{
+      watch:{ // Use watch to check any change in any file down list
 
-          jade: {
+          jade: { // check all changes made on Jade Files
             files: ['./jade/*.jade'],
             tasks: ['jade']
           },
 
-          jshint: {
+          jshint: { // check all changes made on JScript Files
             files: ['./js/*.js'],
             tasks: ['jshint']
           }
 
       },
 
-      jade:{
+      jade:{ // Use this Contrib "Jade" to work HTML Pages
         all:{
           options : {
             pretty : true,
@@ -33,24 +37,21 @@ module.exports = function(grunt) {
         } 
       },
 
-      jshint: {
+      jshint: { // Use this Contrib "JHint" to check all syntax .js files
           all: {
               src: './js/*.js',
           }
       }
-
-  
-
-
   });
 
 
-  
+  // Load all Task
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
 
+  // Registry Main Default Tasks
   grunt.registerTask('default',['jade','watch']);
 
   return;
